57512a78cdd21d2432941cccb06f543d351b77f4
taskinnasif144 <taskinnasif144@gmail.com>
2025-03-19 11:20:45 +0600

    add: implement CustomInputField component with validation and password visibility toggle

---
+++ lib/core/components/custom_input_field.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:joura_pothole/core/constants/app_colors.dart';
+import 'package:joura_pothole/core/constants/app_fonts.dart';
+import 'package:joura_pothole/core/constants/svg_icons.dart';
+import 'package:joura_pothole/core/helpers/input_validators.dart';
+
+class CustomInputField extends StatelessWidget {
+  CustomInputField({
+    super.key,
+    this.isEmail = false,
+    this.isPassword = false,
+    this.title,
+    this.prefixIcon,
+    this.maxLines = 1,
+  });
+
+  final String? title;
+  final bool isEmail;
+  final bool isPassword;
+  final String? prefixIcon;
+  final int maxLines;
+
+  // To track obscure and unobsure state of password text
+  final RxBool isObscure = true.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // the title with the sizedbox will only be rendered when it is provided
+        if (title != null) Text(title!, style: AppFonts.bodyBoldMedium),
+        if (title != null) SizedBox(height: 8.h),
+        // To design the text field container is used
+        Obx(() {
+          final obscure = isObscure.value;
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 4.h),
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.whiteE4),
+            ),
+            // Row is used for prefix icons and suffix icons
+            child: Row(
+              crossAxisAlignment:
+                  maxLines == 1
+                      ? CrossAxisAlignment.center
+                      : CrossAxisAlignment.start,
+              children: [
+                if (prefixIcon != null)
+                  SvgPicture.asset(prefixIcon!, height: 20.h, width: 20.w),
+                if (prefixIcon != null) SizedBox(width: 12.w),
+                Flexible(
+                  child: TextFormField(
+                    // To remove default styling and setting hint text
+                    decoration: InputDecoration(
+                      hintText: "Enter your $title...",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.zero,
+                    ),
+
+                    obscureText: isPassword ? obscure : false,
+
+                    //  The maxlines determines the number of lines that can occupy the field
+                    maxLines: maxLines,
+                    // This is to validate fields based conditions
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        // if the value of field is empty
+                        return "This field cannot be empty";
+                      }
+                      if (isEmail) {
+                        if (!InputValidators.isValidEmail(value)) {
+                          // if email and not validated then the following the error will be shown
+                          return "Please Enter valid $value";
+                        }
+                      }
+                      if (isPassword) {
+                        if (value.length < 8) {
+                          // password validating on length
+                          return "Please Enter valid $value";
+                        }
+                      }
+                      // default return of null
+                      return null;
+                    },
+                  ),
+                ),
+                // for obscure and unobscure text
+                if (isPassword) SizedBox(width: 12.h),
+                if (isPassword)
+                  Obx(() {
+                    return InkWell(
+                      borderRadius: BorderRadius.circular(100),
+                      onTap: () {
+                        isObscure.value = !isObscure.value;
+                      },
+                      child: SvgPicture.asset(
+                        isObscure.value
+                            ? SvgIcons.eyeLineClosed
+                            : SvgIcons.eyeLine,
+                        height: 20.h,
+                        width: 20.w,
+                      ),
+                    );
+                  }),
+              ],
+            ),
+          );
+        }),
+      ],
+    );
+  }
+}
